7637f19480a4b115d1d1d9133f158fa692960778
ChrisCamille <kgrembecki86@gmail.com>
2017-01-05 17:47:23 +0100

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber=99;
+		short myShort=7390;
+		long cat=19982;
+		
+		double myDouble=3.141612;
+		float myFloat=223.34f;
+		
+		char myChar='z';
+		
+		boolean myBoolean=true;
+		
+		byte myByte=-128;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(cat);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+}
